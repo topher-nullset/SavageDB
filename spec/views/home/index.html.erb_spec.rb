66e0d910d5e6f0bb18eedbc998ce424b52a6e108
Chris Cullinane <chriscullinane@protonmail.com>
2024-03-20 03:03:32 -0800

    adds capybara

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'home/index.html.erb', type: :view do
+  it 'displays the home page' do
+    render
+    expect(rendered).to include('Welcome to the Image Gallery')
+  end
 end
